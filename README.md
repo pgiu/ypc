cec57337997ebb68cd4fbe53845f2ddea07882d7
pgiu <pablogiudice@gmail.com>
2017-02-04 20:01:30 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# YouTube Playlist Creator (YPC)
+
+YPC is a simple webapp that lets you create a Youtube playlist based on a list of URLs. 
+
+## How to run it 
+
+There's currently one instance working on this server: 
+
+	http://www-scf.usc.edu/~giudice/ypc/
+	
+
+## How to create your own instance
+
+Prerequisite: 
+
+- A server with a public IP. 
+
+
+1. Clone this repository on your server
+2. Go to `https://console.developers.google.com/projectselector/apis/credentials` and request a YouTube client ID. Remember to add your server to the `Authorized JavaScript origins` list.
+3. Create a file named `oauth2_client_id.js` and place a line like this: 
+	var OAUTH2_CLIENT_ID = 'your_API_Client_ID';
+4. That's all! Go to your server URL and test it.
+
+## How to use it
+
+1. Authorize the application to access your YouTube account (this is safe! No info is collected whatsoever)
+2. Set the name of the new list
+3. Paste one URL per line: `https://www.youtube.com/watch?v=dQw4w9WgXcQ`
+4. Click on "Add all videos to the list"
+5. That's all! Remember that your playlist is set as "private" by default.
+
+## To-Do
+
+- Make one call per box (currently one call is sent per video)
+- Translate the UI to english 
+- add a button to set the visibility to Public
+- add a "delete playlist" button 
---
+++ playlist_updates.js
@@ -0,0 +1,83 @@
+// Define some variables used to remember state.
+var playlistId, channelId;
+
+// After the API loads, call a function to enable the playlist creation form.
+function handleAPILoaded() {
+    enableForm();
+}
+
+// Enable the form for creating a playlist.
+function enableForm() {
+    $('#playlist-button').attr('disabled', false);
+}
+
+// Create a private playlist.
+function createPlaylist() {
+    var name = $('#playlist-name').val()
+    var request = gapi.client.youtube.playlists.insert({
+        part: 'snippet,status',
+        resource: {
+            snippet: {
+                title: name,
+                description: ''
+            },
+            status: {
+                privacyStatus: 'private'
+            }
+        }
+    });
+    request.execute(function(response) {
+        var result = response.result;
+        if (result) {
+            playlistId = result.id;
+            $('#playlist-id').val(playlistId);
+            $('#playlist-title').html(result.snippet.title);
+            $('#playlist-description').html(result.snippet.description);
+        } else {
+            $('#status').html('Could not create playlist');
+            alert('No se pudo crear la playlist');
+        }
+    });
+}
+
+// Add a video ID specified in the form to the playlist.
+function addVideoToPlaylist() {
+    var lines = $('#video-id').val().split('\n');
+
+    for(var i = 0 ; i < lines.length ; i++){
+        var video_id = lines[i].split('=');
+        if (video_id.length == 2){
+            console.log("Adding ("+i+"/"+lines.length+"):"+video_id[1]);
+            addToPlaylist(video_id[1]);
+        } else {
+            console.log("there is more than one =")
+        }
+
+    }
+}
+
+// Add a video to a playlist. The "startPos" and "endPos" values let you
+// start and stop the video at specific times when the video is played as
+// part of the playlist. However, these values are not set in this example.
+function addToPlaylist(id, startPos, endPos) {
+    var details = {
+        videoId: id,
+        kind: 'youtube#video'
+    }
+
+    var request = gapi.client.youtube.playlistItems.insert({
+        part: 'snippet',
+        resource: {
+            snippet: {
+                playlistId: playlistId,
+                resourceId: details
+            }
+        }
+    });
+    var v = request.execute(function(response) {
+        var title = response.result.snippet.title;
+        var img = "<img src=\"" + response.result.snippet.thumbnails.default.url + "\"/>";
+        $('#addedVideos').append('<li>' + img + title + '</li>');
+    });
+    console.log(v);
+}
